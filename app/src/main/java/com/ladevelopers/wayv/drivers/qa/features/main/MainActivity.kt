331de992dcd7b4db6dc02bebd1254c9975f352e6
Andrey Glushko <andrey.glushko@ladevelopers.com>
2018-02-03 11:58:51 +0200

    create project

---
+++ app/src/main/java/com/ladevelopers/wayv/drivers/qa/features/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ladevelopers.wayv.drivers.qa.features.main
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.ladevelopers.wayv.drivers.qa.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
